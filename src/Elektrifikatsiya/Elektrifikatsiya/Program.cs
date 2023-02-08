6abd11ac86ab48a70569b850387d294db4d59886
Stefan-5422 <32109571+Stefan-5422@users.noreply.github.com>
2023-02-08 08:03:51 +0100

    🧾Whoop Whoop Database Time 😎

--- src/Elektrifikatsiya/Elektrifikatsiya/Program.cs
+++ src/Elektrifikatsiya/Elektrifikatsiya/Program.cs
@@ -1,12 +1,15 @@
 using Blazorise;
 using Blazorise.Icons.Material;
 using Blazorise.Material;
+using Elektrifikatsiya.Database;
+using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<DeviceManagmentDatabaseContext>((options)=>options.UseSqlite("Data Source=./DeviceManagement.sqlite"));
 
 AddBlazorise(builder.Services);
 
@@ -31,6 +34,11 @@ app.UseEndpoints(endpoints =>
     _ = endpoints.MapBlazorHub();
     _ = endpoints.MapFallbackToPage("/_Host");
 });
+
+app.MapControllers();
+
+AsyncServiceScope scope = app.Services.CreateAsyncScope();
+scope.ServiceProvider.GetRequiredService<DeviceManagmentDatabaseContext>().Database.EnsureCreated();
 
 app.Run();
 
