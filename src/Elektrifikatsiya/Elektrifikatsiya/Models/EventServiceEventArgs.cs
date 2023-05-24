b054b9cfe33912c9ea48f37e49c5876d75adb84a
Friend2868 <90833416+Friend2868@users.noreply.github.com>
2023-05-24 11:16:18 +0200

    EventService finish

---
+++ src/Elektrifikatsiya/Elektrifikatsiya/Models/EventServiceEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace Elektrifikatsiya.Models
+{
+	public class EventServiceEventArgs : EventArgs
+	{
+		public Event NewEvent { get; set; } = new Event();
+		public EventServiceEventArgs(Event newEvent)
+		{
+			NewEvent = newEvent;
+		}
+	}
+}
--- src/Elektrifikatsiya/Elektrifikatsiya/Pages/Dashboard.razor.cs
+++ src/Elektrifikatsiya/Elektrifikatsiya/Pages/Dashboard.razor.cs
@@ -10,88 +10,83 @@ namespace Elektrifikatsiya.Pages;
 
 public partial class Dashboard
 {
-    //TODO: insert new event here if plug produces one
-    private readonly List<Event> events = new List<Event>() { new Event("Placeholder", "Placeholder", DateTime.Now), new Event("Placeholder", "Placeholder", DateTime.Now), new Event("Placeholder", "Placeholder", DateTime.Now) };
+	//TODO: insert new event here if plug produces one
+	List<Event> events = new List<Event>() { new Event("Text", "Text", DateTime.Today, null ) };
+	//TODO: insert new Device here if user adds one
+	List<Device> plugs = new List<Device>();
+	//code for graph
+	LineChart<double> lineChart;
 
-    //TODO: insert new Device here if user adds one
-    private List<Device> plugs = new List<Device>();
+	protected override void OnInitialized()
+	{
+		plugs = DeviceStatusService.GetDevices().ValueOrDefault ?? new List<Device>();
+		events = EventService.Events;
 
-    //code for graph
-    private LineChart<double> lineChart;
+		EventService.OnEventCalled += (_, e) =>
+		{
+			_ = InvokeAsync(StateHasChanged);
+		};
+		DeviceStatusService.OnDeviceStatusChanged += (_, e) =>
+		{
+			_ = InvokeAsync(StateHasChanged);
+		};
+	}
 
-    protected override void OnInitialized()
-    {
-        plugs = DeviceStatusService.GetDevices().ValueOrDefault ?? new List<Device>();
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		if (firstRender)
+		{
+			await HandleRedraw();
+		}
+	}
 
-        DeviceStatusService.OnDeviceStatusChanged += (_, e) =>
-        {
-            _ = InvokeAsync(StateHasChanged);
-        };
-    }
+	async Task HandleRedraw()
+	{
+		labels.Clear();
+		await lineChart.Clear();
+		await lineChart.AddLabelsDatasetsAndUpdate(labels, await GetLineChartDataset());
+	}
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            await HandleRedraw();
-        }
-    }
+	//TODO: insert dataset of current and last voltage usages
+	async Task<LineChartDataset<double>> GetLineChartDataset()
+	{
+		return new LineChartDataset<double>
+		{
+			Label = "Wattage",
+			Data = await RandomizeData(),
+			Fill = true,
+			PointRadius = 3,
+			CubicInterpolationMode = "monotone",
+		};
+	}
 
-    private async Task Switch(Device device)
-    {
-        device.Enabled = !device.Enabled;
-        _ = await DeviceManagmentService.UpdateDevice(device);
-    }
+	List<string> labels = new List<string>();
 
-    private async Task HandleRedraw()
-    {
-        labels.Clear();
-        await lineChart.Clear();
-        await lineChart.AddLabelsDatasetsAndUpdate(labels, await GetLineChartDataset());
-    }
+	async Task<List<double>> RandomizeData()
+	{
+		if (plugs.Count == 0)
+		{
+			return new();
+		}
+		PrometheusQuery promQueryer = new PrometheusQuery("http://localhost:9090");
 
-    //TODO: insert dataset of current and last voltage usages
-    private async Task<LineChartDataset<double>> GetLineChartDataset()
-    {
-        return new LineChartDataset<double>
-        {
-            Label = "Wattage",
-            Data = await DeviceData(),
-            Fill = true,
-            PointRadius = 3,
-            CubicInterpolationMode = "monotone",
-        };
-    }
+		string plugnames = "";
+		foreach (Device device in plugs)
+		{
+			plugnames += $"shellyplug-s-{device.MacAddress}/relay/0|";
+		}
+		plugnames = plugnames[0..^1];
 
-    private readonly List<string> labels = new List<string>();
+		Debug.WriteLine($$"""sum(power{sensor=~"{{plugnames}}"})[1h:1m]""");
 
-    private async Task<List<double>> DeviceData()
-    {
-        PrometheusQuery promQueryer = new PrometheusQuery("http://localhost:9090");
+		PrometheusDataWrapper? deviceData = (await promQueryer.Query($$"""sum(power{sensor=~"{{plugnames}}"})[1h:1m]"""))?.Data;
 
-        StringBuilder builder = new();
-        foreach (Device device in plugs)
-        {
-            _ = builder.Append($"shellyplug-s-{device.MacAddress}/relay/0|");
-        }
+		var r = new Random(DateTime.Now.Millisecond);
 
-        if (builder.Length <= 1)
-        {
-            return new();
-        }
-
-        string plugnames = builder.ToString()[..^1];
-
-        Debug.WriteLine($$"""sum(power{sensor=~"{{plugnames}}"})[1h:1m]""");
-
-        PrometheusDataWrapper? deviceData = (await promQueryer.Query($$"""sum(power{sensor=~"{{plugnames}}"})[1h:1m]"""))?.Data;
-
-        Random r = new Random(DateTime.Now.Millisecond);
-
-        return deviceData?.MatrixTypeToTimestampFloatTuple().ValueOrDefault?.Select(x =>
-        {
-            labels.Add(DateTimeOffset.FromUnixTimeSeconds(long.Parse($"1{x.Item1}0")).UtcDateTime.ToLocalTime().ToShortTimeString());
-            return x.Item2;
-        }).ToList() ?? new List<double>();
-    }
+		return deviceData?.MatrixTypeToTimestampFloatTuple().ValueOrDefault?.Select(x =>
+		{
+			labels.Add(DateTimeOffset.FromUnixTimeSeconds(long.Parse($"1{x.Item1}0")).UtcDateTime.ToLocalTime().ToShortTimeString());
+			return x.Item2;
+		}).ToList() ?? new List<double>();
+	}
 }
