d5f5ec2d0e2000af9e481ecbf12f6df5d4f7cd88
Friend2868 <90833416+Friend2868@users.noreply.github.com>
2023-06-01 15:21:37 +0200

    Final

--- src/Elektrifikatsiya/Elektrifikatsiya/Program.cs
+++ src/Elektrifikatsiya/Elektrifikatsiya/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddSingleton<INotificationService, NotifcationService>();
 builder.Services.AddSingleton<IScheduledService>(s => s.GetRequiredService<IUpdateService>());
 builder.Services.AddSingleton<IScheduledService>(s=>s.GetRequiredService<INotificationService>());
 builder.Services.AddSingleton<IEmailService, EmailService>();
+builder.Services.AddSingleton<IEventService, EventService>();
 builder.Services.AddSingleton<IHiveMQClient, HiveMQClient>((provider)=>
 {
 	HiveMQClientOptions options = new()
