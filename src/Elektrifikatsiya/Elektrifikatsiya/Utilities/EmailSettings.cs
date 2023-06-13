06e011cb830e059a4fba3f14fa2da3a2f693a4ad
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2023-06-13 22:50:04 +0200

    Fix warning

--- src/Elektrifikatsiya/Elektrifikatsiya/Utilities/EmailSettings.cs
+++ src/Elektrifikatsiya/Elektrifikatsiya/Utilities/EmailSettings.cs
@@ -4,20 +4,20 @@ namespace Elektrifikatsiya.Utilities;
 
 public class EmailSettings
 {
-	public string User { get; set; }
-	public string DefaultEmail { get; set; }
-	public string Key { get; set; }
-	public Dictionary<string, string> Templates { get; set; }
-	public string SmtpServer { get; set; }
-	public int SmtpPort { get; set; }
+    public string User { get; set; }
+    public string DefaultEmail { get; set; }
+    public string Key { get; set; }
+    public Dictionary<string, string> Templates { get; set; }
+    public string SmtpServer { get; set; }
+    public int SmtpPort { get; set; }
 
-	public Dictionary<string, HandlebarsTemplate<object, object>> CompiledTemplates = new();
-	
-	public void CompileTemplates()
-	{
-		foreach ((string templateName, string templatePath) in Templates)
-		{
-			CompiledTemplates.Add(templateName, Handlebars.Compile(File.ReadAllText(templatePath)));
-		}
+    public Dictionary<string, HandlebarsTemplate<object, object>> CompiledTemplates { get; } = new();
+
+    public void CompileTemplates()
+    {
+        foreach ((string templateName, string templatePath) in Templates)
+        {
+            CompiledTemplates.Add(templateName, Handlebars.Compile(File.ReadAllText(templatePath)));
+        }
     }
 }
